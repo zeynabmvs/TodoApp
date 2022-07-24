9d03b6f31d46821a6aa80008698c97fdb1cd9b9c
zeynab <zeynab.mvs@gmail.com>
2022-07-24 12:44:10 +0430

    dark/light toggle done

--- app/js/script.js
+++ app/js/script.js
@@ -1,6 +1,5 @@
-console.log("hello from script.js");
-
 updateCount();
+console.log(localStorage.getItem('colorMode'));
 
 // Function to count uncompleted items
 function updateCount(jQuery) {
@@ -62,7 +61,7 @@ $("input[type='text']").keypress(function (e) {
 // Check off Specific task By Clicking
 $(".all_tasks").on("click", ".task", function () {
     const task = $(this)
-    const checkbox = (this).children('.checkbox').prop( "checked", true );
+    const checkbox = (this).children('.checkbox').prop("checked", true);
     $(this).toggleClass("active checked");
 });
 
@@ -77,3 +76,47 @@ $(".all_tasks").on("click", ".task", function () {
 //         // $(this).parent().find('.task').toggleclass("active checked");
 //     }
 // });
+
+// console.log(localStorage.getItem('colorMode'));
+
+
+const darkButton = document.getElementById('dark');
+const lightButton = document.getElementById('light');
+
+const setDarkMode = () => {
+  document.querySelector('body').classList = 'dark';
+  localStorage.setItem('colorMode', 'dark');
+};
+
+const setLightMode = () => {
+  document.querySelector('body').classList = 'light';
+  localStorage.setItem('colorMode', 'light');
+};
+
+const colorModeFromLocalStorage = () => {
+  return localStorage.getItem('colorMode');
+};
+
+const colorModeFromPreferences = () => {
+  return window.matchMedia('(prefers-color-scheme: dark)').matches 
+              ? 'dark'
+              : 'light' // If preference is set or does not match anything (light is default)
+};
+
+const loadAndUpdateColor = () => {
+  // local storage has precendence over the prefers-color-scheme
+  const color = colorModeFromLocalStorage() || colorModeFromPreferences();
+
+  
+  color == 'dark' ? darkButton.click() : lightButton.click();
+};
+
+// when the prefers-color-scheme changes, this event will be emitted
+// event reflects the media query, if it matches, the new color is dark, else it is light
+window.matchMedia('(prefers-color-scheme: dark)')
+      .addEventListener('change', (event) => {
+        event.matches ? darkButton.click() : lightButton.click();
+      });
+      
+// Load the right color on startup - localStorage has precedence
+loadAndUpdateColor();
